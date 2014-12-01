6ad3917e179570fa766effbd5a69ec816b6c6799
Klymn <jaymondo_007@hotmail.co.uk>
2014-12-01 17:30:03 +0000

    C# RPG Text Based Game

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int strength = 10;
+            int agility = 10;
+            int intelligence = 10;
+            int experience = 0;
+            int level = 0;
+
+            Console.WriteLine("What speciality do you prefer? \n1: Strength \n2: Agility \n3: Intelligence");
+            Console.ReadLine();
+        }
+    }
+}
